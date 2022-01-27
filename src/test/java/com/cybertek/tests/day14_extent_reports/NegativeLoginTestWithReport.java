58bd6663d1b9c07405a4747706055b8c03f9eec4
Vali Tugui <vali.tuguii@gmail.com>
2022-01-27 17:06:41 +0000

    day16: ExcelUtil added

--- src/test/java/com/cybertek/tests/day14_extent_reports/NegativeLoginTestWithReport.java
+++ src/test/java/com/cybertek/tests/day14_extent_reports/NegativeLoginTestWithReport.java
@@ -1,41 +1,56 @@
-package com.cybertek.tests.day13_pom;
+package com.cybertek.tests.day14_extent_reports;
 
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
-import com.cybertek.utilities.ConfigurationReader;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTest extends TestBase {
+public class NegativeLoginTestWithReport extends TestBase {
 
     LoginPage loginPage;
 
     @Test
     public void wrongPasswordTest(){
 
+        //name of the test
+        extentLogger = report.createTest("Wrong password test");
         loginPage= new LoginPage();
 
+        extentLogger.info("Enter username: user1");
         loginPage.usernameInput.sendKeys("user1");
+
+        extentLogger.info("Enter password: somepassword");
         loginPage.passwordInput.sendKeys("somepassword");
+
+        extentLogger.info("CLick in login Button");
         loginPage.loginInput.click();
 
+        extentLogger.info("Verify Page URL");
         Assert.assertEquals(driver.getCurrentUrl(), "https://qa1.vytrack.com/user/login");
+
+        extentLogger.pass("Wrong Password Test is Passed");
 
     }
 
     @Test
     public void wrongUsernameTest(){
+
+        //name of the test
+        extentLogger = report.createTest("Wrong usrname test");
         loginPage = new LoginPage();
 
+        extentLogger.info("Enter username: someusername");
         loginPage.usernameInput.sendKeys("someusername");
+
+        extentLogger.info("Enter password: UserUser123");
         loginPage.passwordInput.sendKeys("UserUser123");
+
+        extentLogger.info("CLick in login Button");
         loginPage.loginInput.click();
 
+        extentLogger.info("Verify Page URL");
         Assert.assertEquals(driver.getCurrentUrl(), "https://qa1.vytrack.com/user/login");
 
+        extentLogger.pass("Passed");
     }
-
 }
-
---
+++ src/test/java/com/cybertek/tests/day16_ddf/ExcelUtilsDemo.java
@@ -0,0 +1,43 @@
+package com.cybertek.tests.day16_ddf;
+
+import com.cybertek.utilities.ExcelUtil;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilsDemo {
+
+
+    @Test
+    public void readExcelFile(){
+
+    //create object from ExcelUtil
+        // it accepts 2 arguments
+        //Argument1: location of the file(path)
+        //Argument2: sheet that we want to open(sheetName)
+
+        ExcelUtil qa3short = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx", "QA3-short");
+
+        //how many rows are in the sheet
+        System.out.println("qa3short.rowCount() = " + qa3short.rowCount());
+
+        //how many columns in the sheet
+        System.out.println("qa3short.columnCount() = " + qa3short.columnCount());
+
+        //get all column names
+        System.out.println("qa3short.getColumnsNames() = " + qa3short.getColumnsNames());
+
+        //get all data in list of maps
+        List<Map<String, String>> dataList = qa3short.getDataList();
+        /*for (Map<String, String> oneROw : dataList) {
+            System.out.println(oneROw);
+
+        }*/
+
+        //get Nona as a value
+        System.out.println("dataList.get(3).get(\"firstname\") = " + dataList.get(2).get("firstname"));
+
+
+    }
+}
